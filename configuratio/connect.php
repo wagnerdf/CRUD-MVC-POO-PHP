cca66c1e44108b90b30e1329fbc35e53fd3750ca
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-06-21 22:07:22 -0300

    Database connection and configuration

--- configuratio/connect.php
+++ configuratio/connect.php
@@ -1,1 +1,35 @@
-<!-- Database connection -->
+<?php
+
+    define('HOST','localhost');
+    define('DATABASENAME', 'crud-mvc-poo-php');
+    define('USER','root');
+    define('PASSWORD', '159357Admin');
+
+    class Connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+        }
+
+        function connectDatabase()
+        {
+            try
+            {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+
+            }
+            catch (PDOException $e)
+            {
+                echo "Error!".$e->getMessage();
+                die();
+            }
+        }
+
+
+    }
+
+    $testeConnection = new Connect(); 
+?>
+
