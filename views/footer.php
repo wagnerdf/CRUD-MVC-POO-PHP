453c2b0fa114ca70345bf894409e587435936a24
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-06-22 21:25:46 -0300

    Add layout navbar

---
+++ views/footer.php
@@ -0,0 +1,32 @@
+<footer>
+        <ul>
+            <li>
+                <a href="mailto:wagnerandrade_df@hotmail.com">wagnerandrade_df@hotmail.com</a> &nbsp;-&nbsp;
+                <a href="https://www.linkedin.com/in/wagner-andrade-876b6460/" target="_blank">LinkedIn</a>&nbsp;-&nbsp;
+                <a href="https://github.com/wagnerdf" target="_blank">GitHub</a>
+            </li>
+        </ul>
+</footer>
+
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"
+></script>
+ 
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>var purecookieTitle="Cookies.",purecookieDesc="Este site utiliza cookies para te proporcionar uma melhor experiência. Ao Continuar navegando você aceita nossa ",purecookieLink=
+'<a href="https://www.cssscript.com/privacy-policy/" target="_blank">Politica de Privacidade?</a>',purecookieButton="Aceito";function pureFadeIn(e,o)
+{var i=document.getElementById(e);i.style.opacity=0,i.style.display=o||"block",function e(){var o=parseFloat(i.style.opacity);(o+=.02)>1||(i.style.opacity=o,requestAnimationFrame(e))}()}
+function pureFadeOut(e){var o=document.getElementById(e);o.style.opacity=1,function e(){(o.style.opacity-=.02)<0?o.style.display="none":requestAnimationFrame(e)}()}function setCookie(e,o,i)
+{var t="";if(i){var n=new Date;n.setTime(n.getTime()+24*i*60*60*1e3),t="; expires="+n.toUTCString()}document.cookie=e+"="+(o||"")+t+"; path=/"}function getCookie(e)
+{for(var o=e+"=",i=document.cookie.split(";"),t=0;t<i.length;t++){for(var n=i[t];" "==n.charAt(0);)n=n.substring(1,n.length);if(0==n.indexOf(o))return n.substring(o.length,n.length)}return null}
+function eraseCookie(e){document.cookie=e+"=; Max-Age=-99999999;"}function cookieConsent(){getCookie("purecookieDismiss")||(document.body.innerHTML+=
+'<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>'+purecookieTitle+'</a></div><div class="cookieDesc"><p>'+purecookieDesc+" "+purecookieLink+
+'</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">'+purecookieButton+"</a></div></div>",pureFadeIn("cookieConsentContainer"))}function purecookieDismiss()
+{setCookie("purecookieDismiss","1",7),pureFadeOut("cookieConsentContainer")}window.onload=function(){cookieConsent()};</script>
+
--- views/index.php
+++ views/index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="widt=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css" rel="stylesheet" />
     <title>Estudos - clients</title>
 </head>
+
 <body>
+
+    <?php
+    include_once('./views/navbar.php');
+    ?>
     <h1>Lista de clientes</h1>
     <div class="content">
         <table class="table">
@@ -19,17 +31,22 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($resultData as $data): ?>
+                <?php foreach ($resultData as $data) : ?>
                     <tr>
-                        <td><?= $data['id']?></td>
-                        <td><?= $data['name']?></td>
-                        <td><?= $data['email']?></td>
-                        <td><?= $data['phone']?></td>
+                        <td><?= $data['id'] ?></td>
+                        <td><?= $data['name'] ?></td>
+                        <td><?= $data['email'] ?></td>
+                        <td><?= $data['phone'] ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
+
+    
+    <?php
+    include_once './views/footer.php';
+    ?>
 
 </body>
 </html>
